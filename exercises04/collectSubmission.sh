9f1f9e7391a489406c177574da7037cfe30be952
StevenKessler <Steven.Kessler@hhu.de>
2020-05-19 16:51:13 +0200

    .

---
+++ exercises04/collectSubmission.sh
@@ -0,0 +1,4 @@
+# please change to your unikennung. so that if you submit your zip file, we can identify it!
+unikennung=mustermann007 
+rm -f ${unikennung}_exercises04.zip
+zip -r ${unikennung}_exercises04.zip . -x "*.git*" "*cs231n/datasets*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements.txt" ".env/*"
